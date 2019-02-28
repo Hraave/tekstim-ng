966f9181df628441d2a46035622d1333cd740d5b
u53rn4m3123 <unconfigured@null.spigotmc.org>
2019-02-28 15:45:50 +0200

    Tegin mängu alguse

--- Project/src/Main.java
+++ Project/src/Main.java
@@ -1,6 +1,36 @@
+import java.util.Scanner;
+
 public class Main {
 
+    private static Scanner scanner = new Scanner(System.in);
+    private static boolean running = false;
+
     public static void main(String[] args) {
-        System.out.println("tere");
+
+        ////// Programm küsib, kas mängija soovib mängu alustada. //////
+
+        System.out.println("Kas soovite mängu alustada?");
+        String input = scanner.nextLine().toLowerCase();
+        if (input.equals("jah")) {
+            StartGame();
+        }
+
+    }
+
+    private static void StartGame() {
+
+        System.out.println("Mis on teie nimi?");
+        Player.name = scanner.nextLine();
+
+        System.out.println("Tere tulemast, " + Player.name);
+
+        running = true;
+        while (running) {
+            System.out.println("| " + Player.name + "\n| Health: " + Player.health + "\n| Armor: " + Player.armor + "\n----------------------------");
+
+            String input = scanner.nextLine();
+
+        }
+
     }
 }
---
+++ Project/src/Player.java
@@ -0,0 +1,6 @@
+public class Player {
+
+    public static String name;
+    public static int health = 100;
+    public static int armor = 0;
+}
