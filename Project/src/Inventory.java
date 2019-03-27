ecd85aa05846f46a0b3191887bae043359678ec6
Hraave <hraave01@gmail.com>
2019-03-27 18:28:44 +0200

    Update Inventory.java

--- Project/src/Inventory.java
+++ Project/src/Inventory.java
@@ -3,14 +3,25 @@ import java.util.List;
 
 public class Inventory {
 
+    public static final int columns = 5;
+    public static final int rows = 5;
+
     public List<Item> items = new ArrayList<>();
 
     public void Add(Item item) {
-        items.add(item);
+        if (items.size() >= columns * rows) {
+            System.out.println("Inventory is full!");
+        } else {
+            items.add(item);
+            Controller.instance.ToggleInventory();
+            Controller.instance.ToggleInventory();
+        }
     }
 
     public void Remove(Item item) {
         items.remove(item);
+        Controller.instance.ToggleInventory();
+        Controller.instance.ToggleInventory();
     }
 
 }
